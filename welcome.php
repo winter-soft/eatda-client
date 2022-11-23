bbde7eacb5a26d1d06a7c44eacc79582db9e7e7d
ITKOO <itkoo2000@dankook.ac.kr>
2022-11-24 02:06:34 +0900

    feat(#7) : 앱 다운로드 홍보 페이지 개발

---
+++ welcome.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="ko">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta content="IE=edge" http-equiv="X-UA-Compatible">
+    <title>잇다</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta content="대학교 기숙사생들을 위한 공동 배달 서비스" name="description">
+    <meta content="잇다, 공동배달, eatda, 기숙사생 배달, 기숙사생, 단국대, 단국대 기숙사" name="keywords"/>
+    <meta property="og:image" content="http://eat-da.com/assets/img/meta/kakao_og.png"/>
+    <meta property="og:title" content="잇다 : EATDA">
+    <meta property="og:description" content="단국대 기숙사생들 위한 공동 배달앱">
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link rel="shortcut icon" href="../assets/img/icon/favicon.png" type="image/x-icon">
+    <link rel="icon" href="../assets/img/icon/favicon.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="http://eat-da.com/assets/img/app_logo.png"/>
+    <link crossorigin href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet">
+    <link href="assets/css/style.css?<?php echo time() ?>" rel="stylesheet">
+    <link href="assets/css/welcome.css?<?php echo time() ?>" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <span>잇다</span>
+    <a href="http://eat-da.com">
+        <button>앱 다운로드</button>
+    </a>
+</header>
+<section id="intro-section">
+    <p data-aos="zoom-in" class="sub-title">단국대학교 학생들이 만든</p>
+    <div class="content">
+        <p data-aos="zoom-in" class="sticker-text">
+            <span class="text-yellow">기숙사생</span><span class="text-dark-gray">을 위한</span>
+            <img src="assets/img/welcome/new_sticker.png" alt="" class="new-sticker">
+        </p>
+        <p data-aos="fade-right" data-aos-delay="500"><span class="text-white">배달비 0원</span></p>
+        <p data-aos="fade-right" data-aos-delay="1000"><span class="text-white">최소주문금액 0원</span></p>
+        <p data-aos="zoom-in" data-aos-delay="1500"><span class="text-yellow">공동배달 서비스</span></p>
+    </div>
+    <a href="http://eat-da.com">
+        <div class="title" data-aos="zoom-in" data-aos-delay="2000">잇다 : EATDA</div>
+    </a>
+</section>
+<section id="section1">
+    김밥 한줄도 <span class="font-weight-bold">무료배달</span><br>
+    <span class="font-weight-bold">오후 4시 30분</span>까지 주문하고<br>
+    <span class="font-weight-bold">기숙사 로비</span>에서 받아보세요<br>
+</section>
+<section id="section2">
+    <div class="e-card">
+        <p class="sup-title" data-aos="zoom-in">배달비 최소주문금액 0원</p>
+        <p class="title" data-aos="zoom-in">
+            자유롭게 골라주세요,<br>
+            배달비와 최소주문금액이 없으니까요.
+        </p>
+        <img src="assets/img/welcome/section2.png" alt="" data-aos="fade-up">
+    </div>
+</section>
+<section id="section3">
+    <div class="e-card">
+        <p class="sup-title" data-aos="zoom-in">모이면 배달시작</p>
+        <p class="title" data-aos="zoom-in">
+            공동배달 딜로<br>
+            규칙적인 한끼를 시작해보세요
+        </p>
+        <img src="assets/img/welcome/section3.png" alt="" data-aos="zoom-in-up">
+    </div>
+</section>
+<section id="section4">
+    <div class="e-card">
+        <p class="sup-title" data-aos="zoom-in">한곳으로 받는 쉬운 배송</p>
+        <p class="title" data-aos="zoom-in">
+            기숙사별로 선택해<br>
+            로비에서 바로 받아보세요
+        </p>
+        <img src="assets/img/welcome/card-2x.png" alt="" data-aos="fade-up">
+    </div>
+</section>
+<footer>
+    <div>
+        <p class="title">서비스</p>
+        <p class="content">
+            <span>문의하기</span>
+        </p>
+    </div>
+    <div>
+        <p class="title">회사</p>
+        <p class="content">
+            <span>제휴문의</span><br>
+        </p>
+    </div>
+    <div>
+        <p class="title mt-2">단국대학교 창업동아리 pro-seed</p>
+        <p class="content">
+            <span>서비스 이용약관 | 개인정보 처리방침</span><br>
+            <span>2022 © pro-seed all rights reserved</span>
+        </p>
+    </div>
+</footer>
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+
+    function moveAndroidDownloadPage() {
+        location.href = "#";
+    }
+</script>
+</body>
+</html>
+
