4cfe5ffba42e51980c0d2a9abc18c39b59670b39
ITKOO <itkoo2000@dankook.ac.kr>
2022-11-26 16:41:32 +0900

    feat(#16): 메뉴 옵션 노출 및 선택시 장바구니 api 호출 시 전달

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,15 +6,31 @@ function callMenuApi(menuId) {
 }
 
 function setMenu(menu) {
+    let optionHtml = "";
+
+    $.each(menu.menuOptionTitle, function (index, menuOptionTitle) {
+        optionHtml += `<p class="menu-option-title">${menuOptionTitle.titleName}</p>
+        <div class='menu-option-detail'>`;
+        $.each(menuOptionTitle.menuOptionList, function (index, menuOption) {
+            let optionPrice = menuOption.optionPrice > 0 ? `<span class="text-gray">(+ ` + numberFormat(menuOption.optionPrice) + '원)</span>' : '';
+            optionHtml += `<span>${menuOption.optionName} ${optionPrice}
+                        <div class="custom-control custom-checkbox mb-1 option-check-box">
+                        <input type="checkbox" class="custom-control-input menu-option" id="menuOption${menuOption.id}" data-id="${menuOption.id}">
+                        <label class="custom-control-label" for="menuOption${menuOption.id}"></label>
+                    </div></p>`;
+        });
+        optionHtml += `</div>`;
+    });
+
     let menuHtml = `
     <div id="menu" class="txt-black">
         <div class="menu-img-box">
-            <img src="${menu.imageUrl}" alt="">
+            <img src="${menu.menu.imageUrl}" alt="">
         </div>
         <div class="menu-content">
-             <p class="font-weight-bold lg-txt">${menu.name}</p>
+             <p class="font-weight-bold lg-txt txt-black">${menu.menu.name}</p>
             <div class="mt-1">
-                <p><span class="font-weight-bold">가격</span><span class="float-right">${numberFormat(menu.price)}원</span></p>
+                <p><span class="font-weight-bold">가격</span><span class="float-right">${numberFormat(menu.menu.price)}원</span></p>
                 <p><span class="font-weight-bold">수량</span>
                     <span class="float-right quantity-box">
                     <span onclick="minusQuantity()">-</span>
@@ -24,6 +40,9 @@ function setMenu(menu) {
                 </p>
             </div>
             <div class="divider mt-3 mb-2"></div>
+        </div>
+        <div class="menu-option-box">
+            ${optionHtml}
         </div>
     </div>
 `;
@@ -59,7 +78,17 @@ function addMenuToCart(menuId, orderId) {
     const storeId = window.localStorage.getItem("sid");
     const quantity = parseInt($("#quantity").text());
 
-    const response = callAddCartApi(menuId, orderId, quantity, 0);
+    // 옵션 선택한게 있을 경우 데이터 넘겨주기(checked 된 요소 찾아서 data-id get)
+    let menuOptionList = [];
+    $.each($(".menu-option:checked"), function (index, checkedOption) {
+        menuOptionList.push(
+            {
+                "menuOption_id": $(checkedOption).data("id")
+            }
+        );
+    });
+
+    const response = callAddCartApi(menuId, orderId, quantity, menuOptionList);
 
     if (response.status === 200) {
         location.href = `../store/index.php?id=${storeId}`;
@@ -84,13 +113,9 @@ function checkSameOrderWhenPutInCart(orderId) {
     return result;
 }
 
-function callAddCartApi(menuId, orderId, quantity, option_id) {
+function callAddCartApi(menuId, orderId, quantity, optionList) {
     const data = {
-        "menuOptions": [
-            {
-                "menuOption_id": option_id
-            }
-        ],
+        "menuOptions": optionList,
         "quantity": quantity
     };
 
